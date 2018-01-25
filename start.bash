8f3251c255bf0d5c68f333f2f26a7d0042639eee
retomoto <retomoto@yandex.ru>
2018-01-25 17:50:49 +0300

    Update start.bash

--- start.bash
+++ start.bash
@@ -32,3 +32,12 @@ else
 fi
 
 service openvpn restart
+
+#Generate up_s2s.sh
+	echo "#!/bin/bash
+#!/bin/bash
+sleep 10
+/sbin/ip route add default via 10.0.2.1 dev tun0 table 10
+/sbin/ip rule add from 10.0.1.0/24 lookup 10 pref 10" >> /etc/openvpn/up_s2s.sh
+#add chmod
+chmod +x /etc/openvpn/up_s2s.sh
